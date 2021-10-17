a8c9eee47d45953d5509b52ed3982faa4af60231
Dmitry <dmitry@gotskin.ru>
2021-10-17 11:25:38 +0300

    Test 2

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import '../node_modules/antd/lib/style/index.less';
+import './happytheme.less';
+
+
+// App specific
+//import './App.less';
+
+
+//Components
+import { Button } from 'antd';
+import { Row, Col } from 'antd';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Design system</h1>
+      <h3>for Platform V</h3>
+      <div className="colorplate">
+        Sky 1
+      </div>
+      <div className="colorplate">
+        Sky 2
+      </div>
+      <div className="colorplate">
+        Sky 3
+      </div>
+      <Row gutter={[32, 32]}>
+        <Col span={8}>
+          <p>Здесь пример текстовки длиной в 60-70 символов в одной строке, это крайне важно для повышения читаемости на портале и работы с интерфейсом. Обращаем на это правило самое пристальное внимание</p>
+        </Col>
+        <Col span={12}>
+          <Button shape="round" size="large" type="primary">Primary Button</Button>
+          <Button>Default Button</Button>
+        </Col>
+      </Row>
+      <Row gutter={[32, 32]}>
+        <Col span={24}>
+          <Button type="dashed">Dashed Button</Button>
+          <Button type="text">Text Button</Button>
+        </Col>
+      </Row>
+      <Button type="link">Link Button</Button>
     </div>
   );
 }
---
+++ my-app/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.less$/i,
+          use: [
+            {
+              loader: "style-loader",
+            },
+            {
+              loader: "css-loader",
+            },
+            {
+              loader: "less-loader",
+              options: {
+                javascriptEnabled: true,
+                lessOptions: {
+                  strictMath: true,
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+  };
+  
