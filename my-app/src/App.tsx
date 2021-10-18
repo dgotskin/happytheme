9e4c65b4d0616080f8ed63c6f7f35ade0a0e5826
dgotskin <dmitry@gotskin.ru>
2021-10-18 15:38:49 +0300

    Typography add

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,13 +5,95 @@ import './App.less';
 
 
 //Components
-import { Row, Col, Button, Form, Input, Checkbox } from 'antd';
-
+import { Row, Col, Button, Form, Input, Checkbox, Typography, Space, Card } from 'antd';
+const { Title, Text, Link } = Typography;
 
 function App() {
   return (
-    <div className="App boxcard md">
-      Here is
+    <div className="App">
+      {/* ============ Стили тут ============  */}
+      <div className="boxcard-md">
+        <Title>Happy Theme for Design System</Title>
+        <Text>Ant Design (default)</Text>
+        <Text type="secondary">Ant Design (secondary)</Text>
+      </div>
+
+      <Row gutter={[32,32]}>
+        <Col span="6">
+          <div className="boxcard-lg filled">
+            <Space direction="vertical" size="large" >
+
+              <Title level={2}>h2. Ant Design</Title>
+              <Title level={3}>h3. Ant Design</Title>
+              <Title level={4}>h4. Ant Design</Title>
+              <Title level={5}>h5. Ant Design</Title>
+
+
+              <Text>Ant Design (default)</Text>
+              <Text type="secondary">Ant Design (secondary)</Text>
+              <Text type="success">Ant Design (success)</Text>
+              <Text type="warning">Ant Design (warning)</Text>
+              <Text type="danger">Ant Design (danger)</Text>
+              <Text disabled>Ant Design (disabled)</Text>
+              <Text mark>Ant Design (mark)</Text>
+              <Text code>Ant Design (code)</Text>
+              <Text keyboard>Ant Design (keyboard)</Text>
+              <Text underline>Ant Design (underline)</Text>
+              <Text delete>Ant Design (delete)</Text>
+              <Text strong>Ant Design (strong)</Text>
+              <Text italic>Ant Design (italic)</Text>
+              <Link href="https://ant.design" target="_blank">
+                Ant Design (Link)
+              </Link>
+            </Space>
+          </div>
+        </Col>
+        <Col span="6">
+          <div className="boxcard-lg">
+            <Space direction="vertical" size="large" >
+
+              <Title level={2}>h2. Ant Design</Title>
+              <Title level={3}>h3. Ant Design</Title>
+              <Title level={4}>h4. Ant Design</Title>
+              <Title level={5}>h5. Ant Design</Title>
+
+
+              <Text>Ant Design (default)</Text>
+              <Text type="secondary">Ant Design (secondary)</Text>
+              <Text type="success">Ant Design (success)</Text>
+              <Text type="warning">Ant Design (warning)</Text>
+              <Text type="danger">Ant Design (danger)</Text>
+              <Text disabled>Ant Design (disabled)</Text>
+              <Text mark>Ant Design (mark)</Text>
+              <Text code>Ant Design (code)</Text>
+              <Text keyboard>Ant Design (keyboard)</Text>
+              <Text underline>Ant Design (underline)</Text>
+              <Text delete>Ant Design (delete)</Text>
+              <Text strong>Ant Design (strong)</Text>
+              <Text italic>Ant Design (italic)</Text>
+              <Link href="https://ant.design" target="_blank">
+                Ant Design (Link)
+              </Link>
+            </Space>
+          </div>
+        </Col>
+      </Row>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      {/* ============ Конец ============  */}
     </div>
   );
 }
--- my-app/src/dist/App.js
+++ my-app/src/dist/App.js
@@ -3,7 +3,57 @@ exports.__esModule = true;
 require("./happytheme.less");
 // App specific
 require("./App.less");
+//Components
+var antd_1 = require("antd");
+var Title = antd_1.Typography.Title, Text = antd_1.Typography.Text, Link = antd_1.Typography.Link;
 function App() {
-    return (React.createElement("div", { className: "App boxcard md" }, "Here is"));
+    return (React.createElement("div", { className: "App" },
+        React.createElement("div", { className: "boxcard-md" },
+            React.createElement(Title, null, "Happy Theme for Design System"),
+            React.createElement(Text, null, "Ant Design (default)"),
+            React.createElement(Text, { type: "secondary" }, "Ant Design (secondary)")),
+        React.createElement(antd_1.Row, { gutter: [32, 32] },
+            React.createElement(antd_1.Col, { span: "6" },
+                React.createElement("div", { className: "boxcard-lg filled" },
+                    React.createElement(antd_1.Space, { direction: "vertical", size: "large" },
+                        React.createElement(Title, { level: 2 }, "h2. Ant Design"),
+                        React.createElement(Title, { level: 3 }, "h3. Ant Design"),
+                        React.createElement(Title, { level: 4 }, "h4. Ant Design"),
+                        React.createElement(Title, { level: 5 }, "h5. Ant Design"),
+                        React.createElement(Text, null, "Ant Design (default)"),
+                        React.createElement(Text, { type: "secondary" }, "Ant Design (secondary)"),
+                        React.createElement(Text, { type: "success" }, "Ant Design (success)"),
+                        React.createElement(Text, { type: "warning" }, "Ant Design (warning)"),
+                        React.createElement(Text, { type: "danger" }, "Ant Design (danger)"),
+                        React.createElement(Text, { disabled: true }, "Ant Design (disabled)"),
+                        React.createElement(Text, { mark: true }, "Ant Design (mark)"),
+                        React.createElement(Text, { code: true }, "Ant Design (code)"),
+                        React.createElement(Text, { keyboard: true }, "Ant Design (keyboard)"),
+                        React.createElement(Text, { underline: true }, "Ant Design (underline)"),
+                        React.createElement(Text, { "delete": true }, "Ant Design (delete)"),
+                        React.createElement(Text, { strong: true }, "Ant Design (strong)"),
+                        React.createElement(Text, { italic: true }, "Ant Design (italic)"),
+                        React.createElement(Link, { href: "https://ant.design", target: "_blank" }, "Ant Design (Link)")))),
+            React.createElement(antd_1.Col, { span: "6" },
+                React.createElement("div", { className: "boxcard-lg" },
+                    React.createElement(antd_1.Space, { direction: "vertical", size: "large" },
+                        React.createElement(Title, { level: 2 }, "h2. Ant Design"),
+                        React.createElement(Title, { level: 3 }, "h3. Ant Design"),
+                        React.createElement(Title, { level: 4 }, "h4. Ant Design"),
+                        React.createElement(Title, { level: 5 }, "h5. Ant Design"),
+                        React.createElement(Text, null, "Ant Design (default)"),
+                        React.createElement(Text, { type: "secondary" }, "Ant Design (secondary)"),
+                        React.createElement(Text, { type: "success" }, "Ant Design (success)"),
+                        React.createElement(Text, { type: "warning" }, "Ant Design (warning)"),
+                        React.createElement(Text, { type: "danger" }, "Ant Design (danger)"),
+                        React.createElement(Text, { disabled: true }, "Ant Design (disabled)"),
+                        React.createElement(Text, { mark: true }, "Ant Design (mark)"),
+                        React.createElement(Text, { code: true }, "Ant Design (code)"),
+                        React.createElement(Text, { keyboard: true }, "Ant Design (keyboard)"),
+                        React.createElement(Text, { underline: true }, "Ant Design (underline)"),
+                        React.createElement(Text, { "delete": true }, "Ant Design (delete)"),
+                        React.createElement(Text, { strong: true }, "Ant Design (strong)"),
+                        React.createElement(Text, { italic: true }, "Ant Design (italic)"),
+                        React.createElement(Link, { href: "https://ant.design", target: "_blank" }, "Ant Design (Link)")))))));
 }
 exports["default"] = App;
